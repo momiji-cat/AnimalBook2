a5fbd0006bcc5c4e3bd0a0a4d884d78cf95cd375
Momiji <karage.kaenyan@gmail.com>
2019-09-24 15:12:21 +0900

    test

---
+++ app/src/main/java/com/example/animalbook2/TitleFragment.kt
@@ -0,0 +1,32 @@
+package com.example.animalbook2
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_title.*
+
+// TODO: Rename paramenter arguments, choose names that match
+// the fragment initialization paraments, e.g. ARG_ITEM_NUMBER
+//private const val ARG_PARAM1 = "param1"
+//private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class TitleFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_title, container, false)
+    }
+    fun setTitle(title: String){
+        titleText.text = title
+    }
+
+}
